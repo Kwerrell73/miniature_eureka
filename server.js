e03e3793a063cf23694f32564803aac7a2186cec
Kellie Werrell <kelliewerrell@gmail.com>
2021-05-27 10:27:31 -0700

    removed tests folder * cleaned up server.js and added listener

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path')
+const { notes } = require('./db/db');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,3 +11,15 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.use(express.static('public'));
+
+
+//Set up listener at bottom of file
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}. Press CTRL C to exit.`)
+});
